b8464e046e17cb5bffaf00c37606704a9acedd77
Polly2000 <kuznetsovapolina2020@gmail.com>
2021-12-08 23:12:26 +0300

    Create: Added form validation

--- js/main.js
+++ js/main.js
@@ -84,4 +84,28 @@ $(document).keyup(function (e) {
       modalDialog.removeClass('modal__dialog_visible');
     }
   });
+
+// Обработка форм
+$('.form').each(function () {
+  $(this).validate({
+  errorClass: "invalid",
+  messages: {
+    name: {
+      required: "Please specify your name",
+      minlength: "The name must be at least two letters long",
+    },
+    email: {
+      required: "We need your email address to contact you",
+      email: "Your email address must be in the format of name@domain.com"
+    },
+    phone: {
+      required: "Phone is required"
+    },
+  },
+});
+})
+
+$(document).ready(function() {
+  $('.phone').mask('+7(999)999-99-99');
+});
 });
